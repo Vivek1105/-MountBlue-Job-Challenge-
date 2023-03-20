9de7f56670279a31bacabdca847eb2f493d7b2bb
Vivek <86347471+Vivek1105@users.noreply.github.com>
2023-03-20 22:28:00 +0530

    Update Beautiful Days at the Movies.py

--- Beautiful Days at the Movies.py
+++ Beautiful Days at the Movies.py
@@ -3,3 +3,34 @@ for _ in range(t):
     parts = list(map(int, input().split(' ')))
     # print('parts', parts)
     print((parts[1] + parts[2] - 2) % parts[0] + 1)
+
+    
+#     C++
+#include <iostream>
+  
+using namespace std;
+
+bool isOk(int x, int mod) {
+    int n = x;
+    int m = 0;
+    while (x > 0) {
+        m = m * 10 + x % 10;
+        x /= 10;
+    }
+    int delta = abs(n - m);
+    delta %= mod;
+    return (delta == 0);
+}
+
+int main() {
+    int l, r, k;
+    cin >> l >> r >> k;
+    int ans = 0;
+    for (int i = l; i <= r; i++) {
+        if (isOk(i, k)) {
+            ++ans;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
