ec715f74e022d31acbf2e27de853ea4bd9869085
Vivek <86347471+Vivek1105@users.noreply.github.com>
2023-03-22 21:14:38 +0530

    Update Picking Numbers.py

--- Picking Numbers.py
+++ Picking Numbers.py
@@ -20,7 +20,19 @@ else:
     print(maxCount)
     
     
-   
+
+# python another approach 
+from collections import Counter
+def pickingNumbers(a):
+        countNums = Counter(a)
+        maxnum=0        
+        for i in range(1, 100):
+             maxnum = max(maxnum, countNums[i]+countNums[i+1])                      
+        
+        return maxnum
+
+
+
 
 # Java code 
 import java.io.*;
