6b1e243cb767ab0cfdc925ffd961ca734ca91ff6
Vivek <86347471+Vivek1105@users.noreply.github.com>
2023-04-05 14:51:54 +0530

    Update Manasa and Stones.py

--- Manasa and Stones.py
+++ Manasa and Stones.py
@@ -7,3 +7,44 @@ if __name__ == '__main__':
         b = int(sys.stdin.readline())
         
         print(*sorted(set(x * a + (n - 1 - x) * b for x in range(n))))
+        
+    
+    
+    
+    
+    
+#  JAVA CODE 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int numberOfTestCases = scan.nextInt();
+        for (int i = 0; i < numberOfTestCases; ++i) {
+            int numberOfStones = scan.nextInt() - 1;
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            
+            if (numberOfStones <= 0) {
+                System.out.println(0);
+            }
+            else {
+                Vector<Integer> results = new Vector<Integer>();
+                for (int j = 0; j <= numberOfStones; ++j) {
+                    results.add((numberOfStones - j) * a + j * b);
+                }
+
+                Collections.sort(results);
+                System.out.print(results.elementAt(0));
+                for (int j = 1; j < results.size(); ++j) {
+                    if (results.elementAt(j).equals(results.elementAt(j - 1))) { continue; }
+                    else { System.out.print(" " + results.elementAt(j)); }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
