1be9367a9d04957313fa4976956f76340d49f5c1
Vivek <86347471+Vivek1105@users.noreply.github.com>
2023-04-12 22:46:17 +0530

    Create Sansa and XOR.py

---
+++ Sansa and XOR.py
@@ -0,0 +1,12 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    ar = [int(i) for i in input().split()]
+    
+    result = 0
+    count = 0
+    for i in range(N):
+        count += N - 2*i
+        if (count) % 2 == 1:
+            result = result ^ ar[i]
+    print(result)
